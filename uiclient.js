be8a8d658cbe5a6cee4bfea4d430cbe3712d524f
Constantin <67434748+costiagur@users.noreply.github.com>
2020-07-05 00:00:14 +0300

    Add files via upload

---
+++ uiclient.js
@@ -0,0 +1,52 @@
+ui = new Object();
+
+ui.host = 'http://localhost:50000';
+
+//*********************************************************************************** */
+
+ui.submit = function(){ //request can be insert or update
+    var xhr = new XMLHttpRequest();
+    var fdata = new FormData();
+
+    fdata.append("in1",document.getElementById("in1").value);
+
+    fdata.append("in2",document.getElementById("in2").value);
+
+    fdata.append("doc1",document.getElementById("doc1").files[0]);
+
+    fdata.append("doc2",document.getElementById("doc2").files[0]);
+
+    fdata.append("doc3",document.getElementById("doc3").files[0]);
+
+    xhr.open('POST',ui.host,true)
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {   
+            resobj = JSON.parse(this.responseText);
+
+            ui.download(resobj.filename,resobj.filetext)
+        }
+    }
+
+    xhr.send(fdata);     
+}
+
+
+//********************************************************************************************* */
+ui.download = function(filename, filetext){
+
+    var a = document.createElement("a");
+
+    document.body.appendChild(a);
+
+    a.style = "display: none";
+
+    a.href = 'data:application/octet-stream;base64,' + filetext;
+
+    a.download = filename;
+
+    a.click();
+
+    document.body.removeChild(a);
+
+}
