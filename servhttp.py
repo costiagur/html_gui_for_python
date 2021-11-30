3a8c7ff58b22df01e6566ad51bf26f98c8acbc38
Constantin <67434748+costiagur@users.noreply.github.com>
2021-11-30 09:07:03 +0200

    Add files via upload

--- servhttp.py
+++ servhttp.py
@@ -1,65 +1,38 @@
 import webserv
 import webbrowser
-#from socketserver import ThreadingMixIn
-#import threading
 import os
-#import backholder
-import http.server
-import myfunc
-import random
 from sys import argv
+import random
 
 HOST = '127.0.0.1'
-PORT = random.randint(50000,60000)
+iniPORT = 50000
+newPORT = random.randint(50000,60000)
+CODESTR = "shirlimirli"
+runningport = iniPORT
+isrepliyed = 0
 
 print(argv)
 
-querystr = ''
-
 if len(argv) == 1:
-    pass
+    querystr = ''
 else:
     querystr = "{" + ",".join(argv[1:]) + "}"
 #
 
 currentfolder =  os.path.dirname(os.path.realpath(__file__))
 
-with open(currentfolder + "/uiclient.js", mode="r", encoding="UTF-8") as jsfile:
-    existingjs = jsfile.readlines() #read all lines from uiclient,js file
-#
-
-with open(currentfolder + "/uiclient.js", mode="w", encoding="UTF-8") as jsfile: #insert ui.host in JS file with random PORT num
-    for jsline in existingjs:
-        if jsline.find("ui.host = 'http://localhost") != -1: #file existing ui.host line and replace it
-            jsfile.write("ui.host = 'http://localhost:%i'\n" %(PORT))
-        #
-        elif jsline.find("ui.queryobj =") != -1:
-            jsfile.write("ui.queryobj = " + querystr + "\n")
-        #
-        else:
-            jsfile.write(jsline) #all other lines write what was there
-        #
-    #
-#
-
-
 htmlfilepath = "file://" + currentfolder + "/index.html"
-
-#class ThreadedHTTPServer(ThreadingMixIn, http.server.HTTPServer):
-#    pass
-#
 
 webbrowser.open(htmlfilepath) #open html file of the UI
 
-webserv.webserv.custmethod = myfunc.myfunc #provide my custom function to POST of webserver
+serv = webserv.HttpServer((HOST,iniPORT),webserv.Handler,CODESTR,newPORT)
 
-#serv = ThreadedHTTPServer((HOST,PORT),webserv.webserv) #threading HTTPserver
-serv = http.server.HTTPServer((HOST,PORT),webserv.webserv)
+while isrepliyed == 0:
+    isrepliyed = serv.run_once()
+    print(isrepliyed)
+#
 
-#server_thread = threading.Thread(target=serv.serve_forever) #preparing thread because tkinter can't run in the same thread with httpserver
-#server_thread.start()
-serv.serve_forever()
+serv.close()
+serv = webserv.HttpServer((HOST,newPORT),webserv.Handler,'',newPORT)
+serv.run_continuously()
 
-#backholder.holderrun("MyUI") #Run Tkinter to hold the server in the background
-
-#serv.shutdown()
--- webserv.py
+++ webserv.py
@@ -1,29 +1,36 @@
 import http.server
-#import backholder
 import post
 from sys import exit
+from myfunc import myfunc
 
-class webserv(http.server.BaseHTTPRequestHandler):
 
-    def processing(self,queryobj,customfunc):
+class Handler(http.server.BaseHTTPRequestHandler):
+    def setcodeword(self, codestr):
+        self.CODESTR = codestr
+        self.REPLIYED = 0
+    #
+
+    def setnewport(self,newPORT):
+        self.newPORT = newPORT
+    #
+
+    def processing(self,queryobj):
         
         postlist = queryobj._POST()
 
         if('request' in postlist.keys()):
             if postlist['request'] == 'close':
-                #backholder.holderclose()
                 exit()
             #
+            elif postlist['request'] == self.CODESTR: 
+                self.REPLIYED = 1
+                return str(self.newPORT).encode()
         #
-        
-        return customfunc(queryobj)
+
+        return myfunc(queryobj)
     #
 
-    def custmethod(self):
-        pass
-    #
-
-    def _set_headers(self):
+    def set_headers(self):
         self.send_response(200) 
         self.send_header('Content-Type', 'text/html')
         
@@ -36,19 +43,49 @@ class webserv(http.server.BaseHTTPRequestHandler):
     #
 
     def do_POST(self):
-        
+
         if self.client_address[0] != '127.0.0.1': #check that request comes from local computer
             return
         #
 
         queryobj = post.POST(self)
 
-        replymsg = self.processing(queryobj,self.custmethod)
+        replymsg = self.processing(queryobj) #,self.custmethod)
 
-        self._set_headers() #set headers of response
+        self.set_headers() #set headers of response
         
         self.wfile.write(replymsg) #send bytes = write to socket
 
         return
+    #
+
+    def isrepliyed(self):
+        return self.REPLIYED
+    #
+#
+
+class HttpServer(http.server.HTTPServer):
+    def __init__(self,address_tuple,useHandler,codestr,newPORT):
+        
+        self.address_tuple = address_tuple
+        self.useHandler = useHandler
+
+        super().__init__(self.address_tuple,self.useHandler)
+        
+        useHandler.setcodeword(useHandler,codestr)
+        useHandler.setnewport(useHandler,newPORT)
+    #
+
+    def run_once(self):      
+        self.handle_request()
+        return self.useHandler.isrepliyed(self.useHandler)
+    #
+    
+    def close(self):
+        self.server_close()
+    #
+
+    def run_continuously(self):
+        self.serve_forever()
     #
 #
