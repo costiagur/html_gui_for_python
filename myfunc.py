1af64606d1cce25b997947ca8dbf870e8f106a96
Constantin <67434748+costiagur@users.noreply.github.com>
2024-01-11 22:06:19 +0200

    Promise, Dialog
    converted to dialog instead of alert
    made promise from ajax request for submission

--- myfunc.py
+++ myfunc.py
@@ -12,20 +12,29 @@ def myfunc(queryobj):
         print("POST = " + str(postdict) + "\n")
         print("FILES = " + str(filesdict) + "\n")
 
-        # reply message should be encoded to be sent back to browser ----------------------------------------------
-        # encoding to base64 is used to send ansi hebrew data. it is decoded to become string and put into json.
-        # json is encoded to be sent to browser.
+        replymsg = ""
 
-        if bool(filesdict):
-            file64enc = base64.b64encode(filesdict['doc1'][1])
-            file64dec = file64enc.decode()
-        
-            replymsg = json.dumps([filesdict['doc1'][0],file64dec]).encode('UTF-8')
+        if postdict["request"] == "num1":
+            res = postdict["in1"]
+            replymsg = json.dumps(["Reply",res]).encode('UTF-8')
         #
-        else: #if filesdict is empty
-            replymsg = json.dumps(["Error","No file provided"]).encode('UTF-8')
+        elif postdict["request"] == "file1":
+
+            # reply message should be encoded to be sent back to browser ----------------------------------------------
+            # encoding to base64 is used to send ansi hebrew data. it is decoded to become string and put into json.
+            # json is encoded to be sent to browser.
+
+            if bool(filesdict):
+                file64enc = base64.b64encode(filesdict['doc1'][1])
+                file64dec = file64enc.decode()
+            
+                replymsg = json.dumps([filesdict['doc1'][0],file64dec]).encode('UTF-8')
+            #
+            else: #if filesdict is empty
+                replymsg = json.dumps(["Error","No file provided"]).encode('UTF-8')
+            #
         #
-        
+                
         return replymsg
     #
     
